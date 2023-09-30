d0775457f9ef1aab11dbcfd152ef65629ed976b3
Noor Zaqir <nzscode@gmail.com>
2023-09-29 20:54:20 -0400

    Update test.py

--- Practice/test.py
+++ Practice/test.py
@@ -1,23 +1,22 @@
-dummy = [
+import csv
+
+dict_demo = [
     {'a': 3, 'b': 8, 'c': 4},
     {'a': 9, 'b': 4, 'c': 2},
-    {'a': 9, 'b': 10, 'c': 6},
+    {'a': 9, 'b': 10, 'c': 6}
 ]
+dict_list = []
+fields = ("k", "v")
 
-dingbat = [
-    {'a': 6, 'b': 4, 'c': 1},
-    {'a': 3, 'b': 8, 'c': 3}
-]
+for i in range(len(dict_demo)):
+    for k, v in dict_demo[i].items():
+        dict_list.append((k, v))
 
-dicty = {}
-lst = []
-for i in range(len(dummy)):
-    lst.append(dummy[i])
+path = "../Resources/"
+with open(path + "dict.csv", 'w') as file:
+    write = csv.writer(file)
 
-for i in range(len(dingbat)):
-    lst.append(dingbat[i])
+    write.writerow(fields)
+    write.writerows(dict_list)
 
-for i in range(len(lst)):
-    dicty[i] = lst[i]
 
-print(dicty)
