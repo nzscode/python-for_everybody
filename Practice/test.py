31fb2f7bcb0319bf7fdcd84b13302e5a1558d497
Noor Zaqir <nzscode@gmail.com>
2023-10-16 21:46:49 -0400

    Update test.py

--- Practice/test.py
+++ Practice/test.py
@@ -41,37 +41,48 @@ alter_table_in_schema = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table R
 # ex: From public to demo_schema
 alter_table_move_schema = """ALTER TABLE IF EXISTS demo_table SET SCHEMA public"""
 alter_table_set_schema = """ALTER TABLE IF EXISTS public.demo_table SET SCHEMA demo_schema"""
-#
-# alter_table_add_column = """ALTER TABLE IF EXISTS demo_table ADD COLUMN IF NOT EXISTS test_column INTEGER;"""
-# alter_table_in_schema_add_column = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ADD COLUMN IF NOT EXISTS test_column INTEGER;"""
-#
-# alter_table_drop_column = """ALTER TABLE IF EXISTS demo_table DROP COLUMN IF EXISTS test_column;"""
-# alter_table_in_schema_drop_column = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table DROP COLUMN IF EXISTS test_column;"""
-#
-# alter_table_rename_column = """ALTER TABLE IF EXISTS demo_table  RENAME COLUMN test_column TO rename_test_column;"""
-# alter_table_in_schema_rename_column = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table RENAME COLUMN test_column TO rename_test_column;"""
-#
-# alter_table_alter_column_type = """ALTER TABLE IF EXISTS demo_table  ALTER COLUMN test_column TYPE VARCHAR(50);"""
-# alter_table_in_schema_alter_column_type = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ALTER COLUMN test_column TYPE VARCHAR(50);"""
-#
-# alter_table_alter_column_default_value = """ALTER TABLE IF EXISTS demo_table  ALTER COLUMN test_column SET DEFAULT 'alien';"""
-# alter_table_in_schema_alter_column_default_value = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ALTER COLUMN test_column SET DEFAULT 'alien';"""
-#
-# alter_table_alter_column_drop_default = """ALTER TABLE IF EXISTS demo_table  ALTER COLUMN test_column DROP DEFAULT;"""
-# alter_table_in_schema_alter_column_drop_default = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ALTER COLUMN test_column DROP DEFAULT;"""
+
+alter_table_add_column = """ALTER TABLE IF EXISTS demo_schema.demo_table ADD COLUMN IF NOT EXISTS l_name VARCHAR(50);"""
+alter_table_in_schema_add_column = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ADD COLUMN IF NOT EXISTS test_column INTEGER;"""
+
+alter_table_drop_column = """ALTER TABLE IF EXISTS demo_table DROP COLUMN IF EXISTS test_column;"""
+alter_table_in_schema_drop_column = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table DROP COLUMN IF EXISTS test_column;"""
+
+alter_table_rename_column = """ALTER TABLE IF EXISTS demo_table  RENAME COLUMN test_column TO rename_test_column;"""
+alter_table_in_schema_rename_column = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table RENAME COLUMN test_column TO rename_test_column;"""
+
+alter_table_alter_column_type = """ALTER TABLE IF EXISTS demo_table  ALTER COLUMN test_column TYPE VARCHAR(50);"""
+alter_table_in_schema_alter_column_type = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ALTER COLUMN test_column TYPE VARCHAR(50);"""
+
+alter_table_alter_column_default_value = """ALTER TABLE IF EXISTS demo_table  ALTER COLUMN test_column SET DEFAULT 'alien';"""
+alter_table_in_schema_alter_column_default_value = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ALTER COLUMN test_column SET DEFAULT 'alien';"""
+
+alter_table_alter_column_drop_default = """ALTER TABLE IF EXISTS demo_table  ALTER COLUMN test_column DROP DEFAULT;"""
+alter_table_in_schema_alter_column_drop_default = """ALTER TABLE IF EXISTS demo_schema.demo_schema_table ALTER COLUMN test_column DROP DEFAULT;"""
 
 # Copy Table
-# copy_table = """ALTER TABLE demo_schema.demo_table ADD COLUMN IF NOT EXISTS (SELECT city, population FROM public.test_table);"""
-#
-# # Insert Values:
+copy_table = """ALTER TABLE demo_schema.demo_table ADD COLUMN IF NOT EXISTS (SELECT city, population FROM public.test_table);"""
+
+# Insert Values:
 insert_values = """INSERT INTO demo_schema.demo_table (id, f_name) VALUES (5, 'Laila');"""
 insert_multiple_values = """INSERT INTO demo_schema.demo_table (id) VALUES (95), (125), (7);"""
 insert_multiple_values_multiple_columns = """INSERT INTO demo_schema.demo_table (id, f_name) VALUES (5, 'Laila'), (41, 'Juno'), (125, 'Yennefer'), (2, 'Ruby' ), (9852, 'Halifa');"""
+
+# Insert Multiple Rows 2
+"""INSERT INTO demo_schema.demo_table (id, l_name) VALUES
+(25, 'Tony'), 
+(64, 'Vince'), 
+(125, 'Zefren'), 
+(8, 'Jorge' ), 
+(295, 'Patel');"""
+
+
 
 # Drop Values:
 drop_rows_where_value = """DELETE FROM demo_schema.demo_table WHERE id = 125;"""
 drop_all_rows = """DELETE FROM demo_schema.demo_table;"""
 drop_multiple_rows = """DELETE FROM demo_schema.demo_table WHERE demo_col_0 IN (9, 7);"""
+drop_rows_value_null = """DELETE FROM demo_schema.demo_table WHERE f_name IS NULL;"""
 
 # Update/Alter Values
 change_single_value = """UPDATE demo_schema.demo_table SET demo_col_1 = 'Toronto' WHERE demo_col_1 = 'Juno';"""
@@ -83,7 +94,7 @@ change_value_to_default = """UPDATE demo_schema.demo_table SET demo_col_1 = DEFA
 copy_values_same_table = """INSERT INTO demo_schema.demo_table(demo_col_2) SELECT demo_col_1 FROM demo_schema.demo_table;"""
 copy_values_different_table = """INSERT INTO demo_schema.demo_schema_table(demo_schema_col_1) SELECT demo_col_1 FROM demo_schema.demo_table;"""
 
-cur.execute(insert_multiple_values)
+cur.execute()
 
 conn.commit()
 conn.close()
